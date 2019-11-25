65ed81541bcfac449182b4d629da3567cbc754d2
rmtar <picpoint@yandex.ru>
2019-11-25 22:40:21 +0300

    make output pictures to pages

--- app.js
+++ app.js
@@ -78,6 +78,25 @@ const storage = new GridFsStorage({
 const uploads = multer({storage});
 
 
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+    if(!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'no file exists'
+      });
+    }
+
+    if(file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'not image'
+      });
+    }
+  });
+});
+
 
 app.get('/records', async (req, res) => {      
   const mass = await Zadvorniy.find({}, (err, docs) => {    
